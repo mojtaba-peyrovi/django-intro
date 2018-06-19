3e38c3c2be1d98fb71ff87a60b248bfe98ebf708
unknown <mojtaba.peyrovi@gmail.com>
2018-06-19 10:50:12 +0700

    endd of part 15

--- music/views.py
+++ music/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
+#from django.template import loader
+from django.shortcuts import render
+from .models import Album
+
 
 def index(request):
-    return HttpResponse("<h1>Music App Home Page</h1>")
+    all_albums = Album.objects.all()
+    # html = ''
+    # for album in all_albums:
+    #     url = '/music/' + str(album.id) + '/'
+    #     html += '<a href="' + url + '">' +  album.album_title +  '</a><br>'
+
+    #template = loader.get_template('music/index.html')
+
+    context = {'all_albums' : all_albums}
+
+    #return HttpResponse(template.render(context,request))
+    return render(request, 'music/index.html', context)
+
+def detail(request,album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+# another way of doing the template is using: from django.shortcuts import render
